f6004daae4018f1d1f8adfab617efff4b245866f
bchimborazo <blanca.chimborazo@stonybrook.edu>
2025-09-06 21:25:27 -0400

    changed definitiondescription to long_common_name

---
+++ scripts/icd10cm_processor.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('input/icd102019syst_codes.txt')
+df.info()
--- scripts/loinc_processor.py
+++ scripts/loinc_processor.py
@@ -3,9 +3,8 @@ df = pd.read_csv('input/Loinc.csv')
 df.info()
 
 df.LOINC_NUM
-df.DefinitionDescription
-
-df_small = df[['LOINC_NUM', 'DefinitionDescription']]
+df.LONG_COMMON_NAME
+df_small = df[['LOINC_NUM', 'LONG_COMMON_NAME']]
 
 df_small
 
@@ -13,6 +12,6 @@ df_small['last_updated'] = '2025-09-06'
 
 df_small
 
-df_small = df_small.rename(columns={'LOINC_NUM': 'Code', 'DefinitionDescription': 'Description'})
+df_small = df_small.rename(columns={'LOINC_NUM': 'code', 'LONG_COMMON_NAME': 'description'})
 df_small
 df_small.to_csv('output/csv/loinc_processed.csv', index=False)
