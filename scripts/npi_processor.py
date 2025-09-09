4d02167026f8ffb4012a177354e13056159ee8fd
bchimborazo <blanca.chimborazo@stonybrook.edu>
2025-09-09 12:17:03 -0400

    5 files complete

--- scripts/npi_processor.py
+++ scripts/npi_processor.py
@@ -1,10 +1,24 @@
-import pandas as pd
-file_path = "input\npidata_pfile_20050523-20250810.csv"
-#loading the first 5 rows to be able to see the column names
-df = pd.read_csv(file_path, nrows=5)
-print("Column names:")
-for i, col in enumerate(df.columns):
-    print(f"{i}: '{col}'")
+import polars as pl
+import time
 
-print("\nFirst 5 rows:")
-print(df.head())
+file_path = "input/npidata_pfile_20050523-20250810.csv"
+df_polars = pl.read_csv(file_path, n_rows=1000000)
+
+print("Columns in the file:")
+print(list(df_polars.columns))
+
+df_polars_small = df_polars.select([
+    'NPI',
+    'Provider Last Name (Legal Name)'
+])
+
+df_polars_small = df_polars_small.with_columns(
+    pl.lit('2025-09-09').alias('last_updated')
+)
+df_polars_small = df_polars_small.rename({
+    'NPI': 'code',
+    'Provider Last Name (Legal Name)': 'description',
+    'last_updated': 'last_updated'
+})
+df_polars_small.write_csv("output/csv/npi_polars_small.csv")
+df_polars_small.write_parquet("output/csv/npi_polars_small.parquet") 
